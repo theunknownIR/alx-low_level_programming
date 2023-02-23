cd6922c866d32ff4d600fa40a8aa0a0b51630a76
Iliass <iliyasrhfir@gmail.com>
2023-02-23 08:00:05 -0800

    from 3 to 10

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
-*print_numbers - function that counts up from 0 to 9
-*@ i: int
-*Return: returns result
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
 */
-
 void print_numbers(void)
 {
-	for (int i = 0; i < 10; i++)
-	{
-		_putchar (i + '0');
-	}
-	_putchar ('\n');
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
 }
