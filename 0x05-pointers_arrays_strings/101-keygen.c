d6ee056faa673d2d1ba06ab36216237330e37be6
Iliass <iliyasrhfir@gmail.com>
2023-02-28 13:40:25 -0800

    advanced

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,26 +1,23 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - generates keygen.
- * Return: 0
+ * main - entry point generates a random password
+ *
+ * Return: the generated password
  */
-
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+char c;
+int x;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
-	return (0);
+srand(time(0));
+while (x <= 2645)
+{
+c = rand() % 128;
+x += c;
+putchar(c);
+}
+putchar(2772 - x);
+return (0);
 }
